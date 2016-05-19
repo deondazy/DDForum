f99d05a9fdc6030c39293e9d3e23531a5ce383b4
Deon Dazy <deondazy@gmail.com>
2016-05-19 12:18:29 +0100

    Fixed user authentication and added a register method

--- core/User.php
+++ core/User.php
@@ -290,6 +290,115 @@ class User
   }
 
   /**
+   * Register user
+   *
+   * @param array $data
+   *   Array of supplied user details
+   *
+   * @return void
+   */
+  public static function register(array $user)
+  {
+    if (!is_array($user) || empty($user)) {
+      throw new \InvalidArgumentException("Argument must be a non empty array");
+    }
+
+    // username is required
+    if (!empty($user['username'])) {
+
+      // Username cannot exceed 16 characters
+      if (strlen($user['username']) <= 16) {
+
+        // Allow only alphanumeric characters, underscores and hyphen
+        if (preg_match("/^[a-zA-Z0-9]+$/", $user['username'])) {
+
+          // Check if username is already taken
+          if (!self::findByName($user['username'])) {
+
+            // email is required
+            if (!empty($user['email'])) {
+
+              // Check if email is valid
+              if (preg_match('/^([a-zA-Z0-9_\.-]+)@([\da-zA-Z0-9_\.-]+)\.([a-zA-Z\.]{2,6})$/', $user['email'])) {
+
+                // Check is email is already taken
+                if (!self::findByEmail($user['email'])) {
+
+                  // Password is required
+                  if (!empty($user['password'])) {
+
+                    // Password confirmation is required
+                    if (!empty($user['password2'])) {
+
+                      // Does the 2 passwords match
+                      if ($user['password'] == $user['password2']) {
+
+                        $first_name = isset($user['first_name']) ? $user['first_name'] : '';
+                        $last_name  = isset($user['last_name']) ? $user['last_name'] : '';
+                        $password   = md5($user['password']);
+
+                        $data = [
+                          'username'      =>  $user['username'],
+                          'password'      =>  $password,
+                          'email'         =>  $user['email'],
+                          'first_name'    =>  $first_name,
+                          'last_name'     =>  $last_name,
+                          'display_name'  =>  $user['username'],
+                          'country'       =>  $user['country'],
+                          'birth_day'     =>  $user['birth_day'],
+                          'birth_month'   =>  $user['birth_month'],
+                          'birth_year'    =>  $user['birth_year'],
+                          'age'           =>  date( 'Y' ) - $_POST['year'],
+                          'gender'        =>  $user['gender'],
+                          'avatar'        =>  self::defaultAvatar(),
+                          'register_date' =>  date('Y-m-d'),
+                          'last_seen'     =>  date('Y-m-d h:i:s'),
+                          'credit'        =>  50, // TODO: Change value to site credit setting
+                        ];
+                      } else {
+                        $error[] = 'Password mismatch';
+                      }
+                    } else {
+                      $error[] = 'Confirm your password';
+                    }
+                  } else {
+                    $error[] = 'Enter your password';
+                  }
+                } else {
+                  $error[] = 'Email is already registered';
+                }
+              } else {
+                $error[] = 'Email is invalid';
+              }
+            } else {
+              $error[] = 'Enter your email';
+            }
+          } else {
+            $error[] = 'Username is already registered';
+          }
+        } else {
+          $error[] = 'Username contains invalid characters, use alphanumeric only';
+        }
+      } else {
+        $error[] = 'Username is too long, 16 alphanumeric characters at most';
+      }
+    } else {
+      $error[] = 'Enter your username';
+    }
+
+    if (!isset($error)) {
+      if (self::create($data)) {
+        $info = 'Registration successful. <a href="'.Site::url().'/login">Login</a>';
+        Site::info($info, false, true);
+      } else {
+        Site::info('Registration failed, try again', true);
+      }
+    } else {
+      Site::info($error);
+    }
+  }
+
+  /**
    * Login user
    *
    * @param string $username
@@ -312,6 +421,8 @@ class User
 
         if ($user) {
           if (md5($password) == $user->password) {
+
+            self::update(['online_status' => 1], $user->userID);
 
             $login_key = md5(self::$loginKey);
 
@@ -351,13 +462,13 @@ class User
    */
   public static function logout()
   {
-    if (isset($_COOKIE['ddforum_logged'])) {
+    if (isset($_COOKIE['ddforum'])) {
 
-      setcookie('ddforum_logged', '', time()-60*60*24*30);
+      setcookie('ddforum', '', time()-60*60*24*30);
 
-      $userId = self::$this->currentUserId();
+      self::update(['online_status' => 0, 'last_seen' => date('Y-m-d H:i:s')], self::currentUserId());
 
-      /**$this->db_obj->update_data( $this->db_obj->users, array( 'online_status' => 0, 'last_seen' => 'now()' ), "userID = '$user_id'");*/
+      return true;
     }
 
     return false;
